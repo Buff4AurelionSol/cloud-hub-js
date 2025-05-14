a3facf5c075397217e6b03a785e42a8c21dfb65f
Deivis Guillen <noctis2001@gmail.com>
2025-05-14 16:54:27 -0400

    style: add animations to table component

--- cloudhub/src/components/Table.vue
+++ cloudhub/src/components/Table.vue
@@ -162,4 +162,21 @@
     align-items: center;
     justify-content: center;
   }
+
+  .box-filters-fade{
+    animation: fade-down 2s normal;
+  }
+
+
+    @keyframes fade-down{
+    from{
+      opacity: 0;
+      transform: translateY(30px) scale(0.9);
+    }
+    to{
+      opacity: 1;
+      transform: translateY(0px) scale(1);
+    }
+  }
+
 </style>
